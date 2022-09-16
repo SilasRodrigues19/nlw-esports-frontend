9195e9abfd3a3161704c2a469d43e50d262a99c6
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-09-16 17:28:25 -0300

    refactor: remove image events

--- src/components/GameBanner.tsx
+++ src/components/GameBanner.tsx
@@ -10,7 +10,7 @@ export const GameBanner = (props: GameBannerProps) => {
       href=''
       className='relative rounded-lg overflow-hidden hover:scale-105 hover:transition-all'
     >
-      <img src={props.bannerUrl} alt='' />
+      <img className='pointer-events-none	' src={props.bannerUrl} alt='' />
       <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute left-0 bottom-0 right-0'>
         <strong className='font-bold text-white block'>{props.title}</strong>
         <span className='text-zinc-300 text-sm block'>
