c9286c5c8ad00223b98c138cb6073c9db91d7dde
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-09-18 20:51:21 -0300

    refactor: adjust images and text after implement swiper

--- src/components/GameBanner.tsx
+++ src/components/GameBanner.tsx
@@ -7,13 +7,19 @@ interface GameBannerProps {
 export const GameBanner = (props: GameBannerProps) => {
   return (
     <a
-      href=''
-      className='relative rounded-lg overflow-hidden hover:scale-105 hover:transition-all'
+      href='javascript:void(0)'
+      className='relative rounded-lg overflow-hidden'
     >
-      <img className='pointer-events-none	' src={props.bannerUrl} alt='' />
+      <img
+        className='pointer-events-none max-h-[15rem] m-auto object-fill'
+        src={props.bannerUrl}
+        alt=''
+      />
       <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute left-0 bottom-0 right-0'>
-        <strong className='font-bold text-white block'>{props.title}</strong>
-        <span className='text-zinc-300 text-sm block'>
+        <strong className='font-bold text-center text-white block'>
+          {props.title}
+        </strong>
+        <span className='text-zinc-300 text-center text-sm block'>
           {props.adsCount} {props.adsCount > 1 ? 'anúncios' : 'anúncio'}
         </span>
       </div>
