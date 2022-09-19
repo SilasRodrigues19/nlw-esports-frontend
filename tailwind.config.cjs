88ee64da3e320f91edc3f051937b31484931ece9
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-09-18 21:36:36 -0300

    refactor: remove galaxy background

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,6 @@ module.exports = {
         sans: ['Inter', 'sans-serif'],
       },
       backgroundImage: {
-        galaxy: "url('/background-galaxy.png')",
         'nlw-gradient':
           'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
         'game-gradient':
