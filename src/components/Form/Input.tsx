0ca618bbc71eb3ba4495da4cc4639d3f4e365296
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-09-19 12:51:11 -0300

    refactor: remove input autocomplete

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -6,6 +6,7 @@ export const Input = (props: InputProps) => {
   return (
     <input
       {...props}
+      autoComplete='off'
       className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'
     />
   );
